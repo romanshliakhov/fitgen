c08feeda79ce0be6d136f1da706ffe3e1adc10d0
Roman <romanshliakhov@gmail.com>
2024-08-22 00:22:12 +0300

    add feedbacks section

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,13 @@
-console.log('test');
-
-
+$('.feedbacks__slider').slick({
+    accessibility: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: true,
+    speed: 2000,
+    prevArrow: '.feedbacks__control--prev',
+    nextArrow: '.feedbacks__control--next',
+});
 
 
 
